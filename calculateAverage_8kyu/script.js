0c0199cbc76b5f3cf203841e61b84f940bf35c78
Stanislau Chernyshevich <trifo7ium@gmail.com>
2022-10-08 13:24:35 +0300

    add descr for 2 task

--- calculateAverage_8kyu/script.js
+++ calculateAverage_8kyu/script.js
@@ -1,3 +1,8 @@
+// DESCRIPTION:
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
 function findAverage(array) {
     let length = array.length;
     let sum = 0;
